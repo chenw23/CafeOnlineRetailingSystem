f47a441bd65e0a9f862908f0abdbe372dc3f0f42
Wang, Chen <zhanghuilian-98022@163.com>
2019-04-13 15:05:02 +0800

    refactor Revert to initial version & add framework

--- src/main/java/fudan/se/lab4/service/impl/OrderServiceImpl.java
+++ src/main/java/fudan/se/lab4/service/impl/OrderServiceImpl.java
@@ -1,6 +1,8 @@
 package fudan.se.lab4.service.impl;
 
+import fudan.se.lab4.dto.Ingredient;
 import fudan.se.lab4.dto.Order;
+import fudan.se.lab4.dto.OrderItem;
 import fudan.se.lab4.dto.PaymentInfo;
 import fudan.se.lab4.service.OrderService;
 import org.springframework.stereotype.Service;
@@ -9,6 +11,21 @@ import org.springframework.stereotype.Service;
 public class OrderServiceImpl implements OrderService {
     @Override
     public PaymentInfo pay(Order order) {
-        return null;
+        double totalPrice = 0;
+        for (OrderItem orderItem : order.getOrderItems()) {
+            totalPrice += 0;
+            for (Ingredient ingredient : orderItem.getIngredients()) {
+                totalPrice += 0;
+            }
+        }
+        //todo: Tang, Xinyue
+        double discount = promotion();
+        return new PaymentInfo(totalPrice, discount,
+                totalPrice * (1 - discount), null);
+    }
+
+    private double promotion() {
+        //todo: Tang, Xinyue
+        return 0;
     }
 }
