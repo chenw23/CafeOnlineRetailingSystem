0e9bbd85bb279e93ac98192a7af2702081d34905
Currycurrycurry <17302010063@fudan.edu.cn>
2019-05-18 14:41:52 +0800

    feat #2262758 Implementation of languageService interface (prototype)

---
+++ src/main/java/fudan/se/lab4/service/LanguageService.java
@@ -0,0 +1,22 @@
+package fudan.se.lab4.service;
+
+public interface LanguageService {
+
+    /**
+     * the language switch
+     * @param language the update language
+     */
+     void updateLanguage(String language);
+
+
+    /**
+     * pass the information name
+     * @param name the name of the information
+     * @return the value in special language
+     */
+     String getValue(String name);
+
+
+
+
+}
