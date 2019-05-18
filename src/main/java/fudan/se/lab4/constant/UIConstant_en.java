87ebdf9995b8cb69fe53a2e36b3cde9789a5c713
Currycurrycurry <17302010063@fudan.edu.cn>
2019-05-18 14:36:07 +0800

    feat #2262754 Implementation of English constant files

---
+++ src/main/java/fudan/se/lab4/constant/UIConstant_en.java
@@ -0,0 +1,22 @@
+package fudan.se.lab4.constant;
+
+public class UIConstant_en {
+    public static final String USER_ALREADY_EXIST = "User already exist,name : {0}";
+    public static final String USER_LOGIN_SUCCESSFULLY = "User login successfully,name : {0}";
+    public static final String USER_SIGNUP_SUCCESSFULLY = "User signup successfully,name : {0}";
+    public static final String USER_LOGIN_FAILED = "User login failed";
+    public static final String USERNAME_OR_PASS_ERROR = "Username or password error.";
+
+
+    public static final String DISCOUNT_CAPPUCCINO = "Cappuccino second half price, discount %d$";
+    public static final String DISCOUNT_TEA = "Tea buy three send one, discount %d$";
+    public static final String DISCOUNT_ESPRESSO = "Large-cup Espresso 2 cups 20% off，discount %d$";
+    public static final String DISCOUNT_FULL_REDUCTION = "All drinks full 100 minus 30, discount %f";
+    public static final String DISCOUNT_Double_Eleven = "Double Eleven all drinks 50% off, discount %f";
+    public static final String DISCOUNT_TeaAndCoffee15OFF = "Order including at least one tea or coffee 15%, discount %f";//订单中包含至少一件茶类商品或至少一件咖啡类商品打85折
+
+
+
+
+
+}
