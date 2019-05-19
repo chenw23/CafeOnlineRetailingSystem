2b7243707c093d95fc8b465cdaf7e562f3da6d77
jiaxingliu <1260968291@qq.com>
2019-05-19 10:38:50 +0800

    fix #2270341 add some codes to the swagger test class.

--- src/main/java/fudan/se/lab4/controller/OrderController.java
+++ src/main/java/fudan/se/lab4/controller/OrderController.java
@@ -2,7 +2,8 @@ package fudan.se.lab4.controller;
 
 import fudan.se.lab4.dto.Order;
 import fudan.se.lab4.dto.PaymentInfo;
-import fudan.se.lab4.service.OrderService;
+import fudan.se.lab4.service.MarketingStrategy;
+import fudan.se.lab4.service.impl.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,16 +11,24 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 import static org.springframework.http.ResponseEntity.ok;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
     @Autowired
-    private OrderService orderService;
+    private OrderServiceImpl orderServiceImpl;
 
     @PostMapping
     public ResponseEntity<PaymentInfo> order(@RequestBody Order order) {
-        return ok().body(orderService.pay(order));
+        ArrayList<MarketingStrategy> strategies = new ArrayList<>();
+        strategies.add(new DoubleElevenStrategy());
+        strategies.add(new CombinationDiscountStrategy());
+        strategies.add(new FullDiscountStrategy());
+        strategies.add(new TeaAndCoffee15OffStrategy());
+        orderServiceImpl.setStrategies(strategies);
+        return ok().body(orderServiceImpl.pay(order));
     }
 }
