1f4a442e9baeac40e8fe4ad953d7a1467209ae94
jiaxingliu <1260968291@qq.com>
2019-05-20 15:43:20 +0800

    fix Reduce repeatability.

--- src/main/java/fudan/se/lab4/constant/UIConstant_cn.java
+++ src/main/java/fudan/se/lab4/constant/UIConstant_cn.java
@@ -2,20 +2,19 @@ package fudan.se.lab4.constant;
 
 public class UIConstant_cn {
     public static final String USER_ALREADY_EXIST = "该用户已存在，用户名：{0}";
-    public static final String USER_LOGIN_SUCCESSFULLY = "用户登录成功,用户名: {0}";
-    public static final String USER_SIGNUP_SUCCESSFULLY = "用户注册成功,用户名: {0}";
-    public static final String USER_LOGIN_FAILED = "用户登录失败";
     public static final String USERNAME_OR_PASS_ERROR = "用户名或密码错误";
-
-    public static final String USER_HAS_LOGED_IN = "用户已登录";
+    public static final String USER_SIGNUP_SUCCESSFULLY = "用户注册成功,用户名: {0}";
+    public static final String USER_LOGIN_SUCCESSFULLY = "用户登录成功,用户名: {0}";
+    public static final String USER_LOGIN_FAILED = "用户登录失败";
     public static final String PLEASE_LOGIN= "请先登录";
+    public static final String USER_HAS_LOGED_IN = "用户已登录";
 
-    public static final String DISCOUNT_CAPPUCCINO = "卡布奇诺第二杯半价，折扣 %f";
-    public static final String DISCOUNT_TEA = "茶买3送1，折扣 %f";
-    public static final String DISCOUNT_ESPRESSO = "大杯浓缩咖啡2杯8折，折扣 %f";
     public static final String DISCOUNT_FULL_REDUCTION = "所有商品满100$省30$,折扣 %f";
     public static final String DISCOUNT_DOUBLE_ELEVEN = "双十一所有商品打五折,折扣 %f";
+    public static final String DISCOUNT_TEA = "茶买3送1，折扣 %f";
+    public static final String DISCOUNT_CAPPUCCINO = "卡布奇诺第二杯半价，折扣 %f";
     public static final String DISCOUNT_TEA_AND_COFFEE_15_OFF = "订单中包含至少一件茶类商品或至少一件咖啡类商品打85折,折扣 %f";
+    public static final String DISCOUNT_ESPRESSO = "大杯浓缩咖啡2杯8折，折扣 %f";
 
 
 }
